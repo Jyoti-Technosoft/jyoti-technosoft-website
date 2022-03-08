0e76d6f6f2b9396290c3b2c118d9e0605d84ad86
dipesh <dipesh_gandhi9@yahoo.com>
2022-03-08 12:08:27 +0530

    Image slider, font stylings

--- src/app/modules/web/components/home/home.component.ts
+++ src/app/modules/web/components/home/home.component.ts
@@ -7,14 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public imagesUrl: any = [];
+
   images = [ 
-    {imagePath: 'app/assets/gallery_images/Camel.jpg'},
-    {imagePath: 'app/assets/gallery_images/dog.jpg'},
-    {imagePath: 'app/assets/gallery_images/Tiger.jpg'}
+    { imagePath: 'app/assets/gallery_images/Camel.jpg' },
+    { imagePath: 'app/assets/gallery_images/dog.jpg' },
+    { imagePath: 'app/assets/gallery_images/Tiger.jpg' }
   ];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.imagesUrl = [
+      { image: 'app/assets/client/biztree.png', thumbImage: 'app/assets/client/biztree.png', title: '' },
+      { image: 'app/assets/client/boatgroup.png', thumbImage: 'app/assets/client/boatgroup.png', title: '' },
+      { image: 'app/assets/client/cx-group.png', thumbImage: 'app/assets/client/cx-group.png', title: '' },
+      { image: 'app/assets/client/enteros.png', thumbImage: 'app/assets/client/enteros.png', title: '' },
+      { image: 'app/assets/client/oxos.png', thumbImage: 'app/assets/client/oxos.png', title: '' },
+      { image: 'app/assets/client/tlc.png', thumbImage: 'app/assets/client/tlc.png', title: '' },
+    ];
   }
 
 }
--- src/app/modules/web/web.module.ts
+++ src/app/modules/web/web.module.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCarouselModule } from 'ng-mat-carousel';
+import { NgImageSliderModule } from 'ng-image-slider';
 
 import { WebRoutingModule } from './web-routing.module';
 import { PageComponent } from './components/page/page.component';
@@ -29,6 +30,7 @@ import { TopNavigatorComponent } from './components/top-navigator/top-navigator.
     MatButtonModule,
     MatCardModule,
     MatGridListModule,
+    NgImageSliderModule,
     WebRoutingModule,
     MatCarouselModule.forRoot()
   ]
