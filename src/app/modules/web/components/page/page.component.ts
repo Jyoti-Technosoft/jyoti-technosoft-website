8427cc1a60e48a88d2b046a1fb265716cc5304de
Dipesh Gandhi <dipesh_gandhi9@yahoo.com>
2022-10-20 11:23:55 +0530

    Fix default selected header as home

--- src/app/modules/web/components/page/page.component.ts
+++ src/app/modules/web/components/page/page.component.ts
@@ -1,34 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { ViewportScroller } from "@angular/common";
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
-  styleUrls: ['./page.component.scss']
+  styleUrls: ['./page.component.scss'],
 })
 export class PageComponent implements OnInit {
-
   title = 'Jyoti Technosoft LLP';
-  section: string = '';
-  public item : any = '';
-  constructor(
-  private scroller: ViewportScroller
-  ) { }
+  section: string = 'home';
+
+  constructor(private scroller: ViewportScroller) {}
 
   ngOnInit(): void {
-    this.item = localStorage.getItem('item') || "";
-    this.showSection(this.item);
+    this.section = localStorage.getItem('item') || 'home';
+    this.showSection(this.section);
   }
-    
+
   showSection(event: string) {
     this.section = event;
-    if(event == 'about' || event == 'contacts' || event == 'home') {
-      this.scroller.setOffset([0, 60])
+    if (event == 'about' || event == 'contacts' || event == 'home') {
+      this.scroller.setOffset([0, 60]);
     } else {
-      this.scroller.setOffset([0, 10])
+      this.scroller.setOffset([0, 10]);
     }
     setTimeout(() => {
       this.scroller.scrollToAnchor(event);
-    }, 50)
+    }, 50);
   }
 }
--- src/app/modules/web/components/top-navigator/top-navigator.component.ts
+++ src/app/modules/web/components/top-navigator/top-navigator.component.ts
@@ -6,23 +6,23 @@ import data from '../../../../../assets/data/JT-website-json-data.json';
 @Component({
   selector: 'app-top-navigator',
   templateUrl: './top-navigator.component.html',
-  styleUrls: ['./top-navigator.component.scss']
+  styleUrls: ['./top-navigator.component.scss'],
 })
 export class TopNavigatorComponent implements OnInit {
-
   @Output() showSection = new EventEmitter<string>();
   logo: any = data.topnavigator[0].logoimage;
-  selectedNav: string = "";
-  constructor( private router: Router ) { }
+  selectedNav: string = 'home';
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.selectedNav = localStorage.getItem('item') || "";
+    this.selectedNav = localStorage.getItem('item') || 'home';
   }
   goDown(section: string) {
     this.selectedNav = section;
     localStorage.setItem('item', section);
-    if(this.router.url.includes('/gallery')){
-      this.router.navigate(['/'])
+    if (this.router.url.includes('/gallery')) {
+      this.router.navigate(['/']);
     }
     this.showSection.emit(section);
   }
