384e58e1c922644bf5ea953208a20378ff3e073c
dipesh <dipesh_gandhi9@yahoo.com>
2022-10-21 13:52:25 +0530

    preloader implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, Inject, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  constructor(@Inject(DOCUMENT) private document: Document,
+  private renderer: Renderer2,) {  }
+
+  ngAfterViewInit() {
+    this.renderer.setStyle(document.getElementById('preloader'), 'display', 'none');
+  }
+
 }
